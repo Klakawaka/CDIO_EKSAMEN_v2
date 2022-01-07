c65433b1f3c46b89d2f9dd2b043fa1ca20b40ccd
Miladibra1 <91473417+Miladibra1@users.noreply.github.com>
2022-01-07 17:16:14 +0100

    nogle Chancekort fra det tildelte dokument er tilføjet, mangler stadig at tilføje resten. De gamle chancekort er sat som kommentar, i tilfælde af de kan bruges

--- src/main/java/ChanceCard.java
+++ src/main/java/ChanceCard.java
@@ -9,14 +9,39 @@ public class ChanceCard {
 
         switch (card) {
             case 1:
-                System.out.println("Du får en gave på 200 dollars");
-                player.account.addNewBalance(+200);
+           System.out.println("You have ran into a full stop, pay 1000 kr in fine");
+                player.account.addNewBalance(-1000);
                 break;
             case 2:
-                System.out.println("Du får en bøde på 200 dollars");
-                player.account.addNewBalance(-200);
+                System.out.println("Pay 300 kr for a car wash and greasing");
+                player.account.addNewBalance(-300);
                 break;
             case 3:
+                System.out.println("Pay 200 kr for delivery of 2 crates of beer");
+                player.account.addNewBalance(-200);
+            case 4:
+                System.out.println("Pay 3000 kr for car repair");
+                player.account.addNewBalance(-3000);
+            case 5:
+                System.out.println("Pay 3000 kr for car repair");
+                player.account.addNewBalance(-3000);
+            case 6:
+                System.out.println("You have bought 4 new tyres for your car, pay 1000 kr");
+                player.account.addNewBalance(-1000);
+            case 7:
+                System.out.println("You have received a parking ticket, pay 200 kr fine");
+                player.account.addNewBalance(-200);
+            case 8:
+                System.out.println("Pay your car insurance, kr 1000");
+                player.account.addNewBalance(-1000);
+            case 9:
+                System.out.println("You have been overseas and bought too many cigarettes, pay 200 kr in customs duty");
+                player.account.addNewBalance(-200);
+            case 10:
+                System.out.println("Dental bill, pay 2000 kr");
+                player.account.addNewBalance(-2000);
+
+                /*
                 System.out.println("Du skal rykke 3 felter frem");
                 player.addpostion(3);
                 break;
@@ -62,6 +87,8 @@ public class ChanceCard {
                 System.out.println("Du får en bøde på 500 dollars");
                 player.account.addNewBalance(-500);
                 break;
+            case 13:
+                System.out.println(""); */
         }
         return card;
     }
