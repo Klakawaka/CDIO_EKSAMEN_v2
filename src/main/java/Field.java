66f55333681f93863ed03ae406fd0075ae42cce3
Klakawaka <91070430+Klakawaka@users.noreply.github.com>
2022-01-11 10:55:16 +0100

    rettelser i field

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -18,7 +18,7 @@ public class Field {
                break;
             case 1:
                 Rødovrevej = 60;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-60);
                 }
                 break;
@@ -27,34 +27,38 @@ public class Field {
                 break;
             case 3:
               Hvidovrevej = 60;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-60);
                 }
               break;
             case 4:
                 betalSkat1 = 200;
                 player.account.addNewBalance(-200);
+                break;
             case 5:
                 Øresund = 200;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-200);
                 }
+                break;
             case 6:
                 Roskildevej = 100;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-100);
                 }
+                break;
             case 7:
                 chanceCard.getChancecard(player);
                 break;
             case 8:
                 Valbylandgade = 100;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-100);
                 }
+                break;
             case 9:
                 Allegade = 120;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-120);
                 }
                 break;
@@ -63,45 +67,52 @@ public class Field {
                 break;
             case 11:
                 FrederiksbergAlle =140;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-140);
                 }
+                break;
             case 12:
                 Tuborg = 150;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-150);
                 }
+                break;
             case 13:
                 Bülowsvej =140;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-140);
                 }
+                break;
             case 14:
                 GammelKongevej = 140;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-140);
                 }
+                break;
             case 15:
                 DFDS = 200;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-200);
                 }
+                break;
             case 16:
                 Bernstofssvej = 180;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-180);
                 }
+                break;
             case 17:
                 chanceCard.getChancecard(player);
                 break;
             case 18:
                 Hellerupsvej = 180;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-180);
                 }
+                break;
             case 19:
                 Strandvejen = 180;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-180);
                 }
                 break;
@@ -110,89 +121,103 @@ public class Field {
                 break;
             case 21:
                 Trianglen = 220;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-220);
                 }
+                break;
             case 22:
                 chanceCard.getChancecard(player);
-
                 break;
             case 23:
               Østerbrogade = 220;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-220);
                 }
+                break;
             case 24:
                 Grøningen = 240;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-240);
                 }
+                break;
             case 25:
                 ØS = 200;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-200);
                 }
+                break;
             case 26:
                 Bredgade = 260;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-260);
                 }
+                break;
             case 27:
                 KongensNytorv = 260;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-260);
                 }
+                break;
             case 28:
                 Carlsberg = 150;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-150);
                 }
+                break;
             case 29:
                 Østergade = 280;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-280);
                 }
+                break;
             case 30:
                 //Fængsel
                 break;
             case 31:
                 Amagertorv = 300;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-300);
                 }
+                break;
             case 32:
                 Vimmelskaftet = 300;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-300);
                 }
+                break;
             case 33:
                 chanceCard.getChancecard(player);
                 break;
             case 34:
                 Nygade = 320;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-320);
                 }
+                break;
             case 35:
                 Bornholm = 200;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-200);
                 }
+                break;
             case 36:
                 chanceCard.getChancecard(player);
             case 37:
                 Frederiksberggade = 350;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-350);
                 }
+                break;
             case 38:
                 betalSkat2 = 100;
                 player.account.addNewBalance(-100);
+                break;
             case 39:
                 Rådhuspladsen = 400;
-                if (x == true) {
+                if (x) {
                     player.account.addNewBalance(-400);
                 }
+                break;
 
         }
 
