2db3f186c1dc206498a7cb22b45a4d3874ce8c2c
Mudi26 <s215772@student.dtu.dk>
2022-01-11 21:21:26 +0100

    Har tilføjet resten af felterne til arrayet i Board klassen

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,8 +1,34 @@
-//public class Board
-//{
-    //Field[] fields = new Field[]{
-            //new Field("Start"),
-            //new Field("Hvidovrevej"),
-            //new Field("Øresund"),
-   // };
-//}
+public class Board {
+    Field[] fields = new Field[]{
+            new Field("Start"),
+            new Field("Hvidovrevej"),
+            new Field("betalSkat1"),
+            new Field("Øresund"),
+            new Field("Roskildevej"),
+            new Field("Valbylandgade"),
+            new Field("Allegade"),
+            new Field("FrederiksbergAlle"),
+            new Field("Tuborg"),
+            new Field("Bülowsvej"),
+            new Field("GammelKongevej"),
+            new Field("DFDS"),
+            new Field("Bernstofssvej"),
+            new Field("Hellerupsvej"),
+            new Field("Strandvejen"),
+            new Field("Trianglen"),
+            new Field("Østerbrogade"),
+            new Field("Grøningen"),
+            new Field("ØS"),
+            new Field("Bredgade"),
+            new Field("KongensNytorv"),
+            new Field("Carlsberg"),
+            new Field("Østergade"),
+            new Field("Amagertorv"),
+            new Field("Vimmelskaftet"),
+            new Field("Nygade"),
+            new Field("Bornholm"),
+            new Field("Frederiksberggade"),
+            new Field("betalSkat2"),
+            new Field("Rådhuspladsen"),
+    };
+}
