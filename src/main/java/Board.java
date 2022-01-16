5c7c77041bbb6dbc790a0bbd47d3277605a583ee
Mudi26 <s215772@student.dtu.dk>
2022-01-16 16:28:01 +0100

    Jeg har flyttet alt det der var i Field klassen som vi ikke skal bruge alligevel til Board klassen.. Bare sådan så det ikke fylder meget i en klasse vi faktisk bruger(field)

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -35,3 +35,98 @@ public class Board {
 }
 
  */
+
+/*
+public class Field {
+    public Player owner;
+    String title;
+
+    public Field(String title) {
+        this.title = title;
+    }
+
+    //Denne linje nedenunder skal være til, ellers er der umiddelbart et problem i Board klassen.
+    public void fields(Player player, int position, boolean buyButton, Player[] playerList) {
+
+    }
+
+    class StreetField extends Field {
+         public int StreetField(int price) {
+
+             return price;
+         }
+
+        public StreetField(String title) {
+            super(title);
+        }
+
+        //StreetField Rødovrevej = new StreetField();
+        //StreetField Hvidovrevej = new StreetField();
+        //StreetField Øresund = new StreetField();
+        //StreetField Roskildevej = new StreetField();
+        //StreetField Valbylandsgade = new StreetField();
+        //StreetField Allégade = new StreetField("Skal det her måske sættes i Board klassen?");
+        //StreetField 8 = new StreetField();
+        //StreetField 9 = new StreetField();
+        //StreetField 10 = new StreetField();
+        //StreetField 11 = new StreetField();
+        //StreetField 12 = new StreetField();
+        //StreetField 13 = new StreetField();
+        //StreetField 14 = new StreetField();
+        //StreetField 15 = new StreetField();
+        //StreetField 16 = new StreetField();
+        //StreetField 17 = new StreetField();
+        //StreetField 18 = new StreetField();
+        //StreetField 19 = new StreetField();
+        //StreetField 20 = new StreetField();
+        //StreetField 21 = new StreetField();
+        //StreetField 22 = new StreetField();
+        //StreetField 23 = new StreetField();
+        //StreetField 24 = new StreetField();
+        //StreetField 25 = new StreetField();
+        //...
+
+
+
+
+        public void buy() {
+            this.owner =  owner;
+        }
+
+
+    }
+
+    class TaxFields extends Field {
+        int tax;
+
+        public TaxFields(String title) {
+            super(title);
+        }
+    }
+
+
+    //public StreetField(String title) {
+    //super(title);
+}
+
+
+        // {
+        // String title;
+        // public Player owner;
+
+        // public Field(String title) {
+        // this.title = title;
+        // }
+        // public void buy (Player x) {
+        // this.owner = x;
+        // }}
+
+
+/*{
+/*String title;
+public Field(String title) {
+   this.title = title;
+
+ */
+
+///int owner;
--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -1,99 +1,3 @@
-/*
-public class Field {
-    public Player owner;
-    String title;
-
-    public Field(String title) {
-        this.title = title;
-    }
-
-    //Denne linje nedenunder skal være til, ellers er der umiddelbart et problem i Board klassen.
-    public void fields(Player player, int position, boolean buyButton, Player[] playerList) {
-
-    }
-
-    class StreetField extends Field {
-         public int StreetField(int price) {
-
-             return price;
-         }
-
-        public StreetField(String title) {
-            super(title);
-        }
-
-        //StreetField Rødovrevej = new StreetField();
-        //StreetField Hvidovrevej = new StreetField();
-        //StreetField Øresund = new StreetField();
-        //StreetField Roskildevej = new StreetField();
-        //StreetField Valbylandsgade = new StreetField();
-        //StreetField Allégade = new StreetField("Skal det her måske sættes i Board klassen?");
-        //StreetField 8 = new StreetField();
-        //StreetField 9 = new StreetField();
-        //StreetField 10 = new StreetField();
-        //StreetField 11 = new StreetField();
-        //StreetField 12 = new StreetField();
-        //StreetField 13 = new StreetField();
-        //StreetField 14 = new StreetField();
-        //StreetField 15 = new StreetField();
-        //StreetField 16 = new StreetField();
-        //StreetField 17 = new StreetField();
-        //StreetField 18 = new StreetField();
-        //StreetField 19 = new StreetField();
-        //StreetField 20 = new StreetField();
-        //StreetField 21 = new StreetField();
-        //StreetField 22 = new StreetField();
-        //StreetField 23 = new StreetField();
-        //StreetField 24 = new StreetField();
-        //StreetField 25 = new StreetField();
-        //...
-
-
-
-
-        public void buy() {
-            this.owner =  owner;
-        }
-
-
-    }
-
-    class TaxFields extends Field {
-        int tax;
-
-        public TaxFields(String title) {
-            super(title);
-        }
-    }
-
-
-    //public StreetField(String title) {
-    //super(title);
-}
-
-
-        // {
-        // String title;
-        // public Player owner;
-
-        // public Field(String title) {
-        // this.title = title;
-        // }
-        // public void buy (Player x) {
-        // this.owner = x;
-        // }}
-
-
-/*{
-/*String title;
-public Field(String title) {
-   this.title = title;
-
- */
-
-
-
- /// Original
 public class Field {
 
     /* Start,Rødovrevej,Hvidovrevej,betalSkat1,Øresund,Roskildevej,
@@ -104,7 +8,6 @@ public class Field {
             Grøningen,ØS,Bredgade,KongensNytorv,Carlsberg,Østergade,
             Amagertorv,Vimmelskaftet,Nygade,Bornholm,
             Frederiksberggade,betalSkat2,Rådhuspladsen;
-    ///int owner;
 
      */
 
